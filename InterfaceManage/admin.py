14377839dfcce5ab691f870c165dc456d481c358
mantodeateam@163.com <mantodeateam@163.com>
2016-02-03 09:17:26 +0800

    add app manage

--- InterfaceManage/admin.py
+++ InterfaceManage/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from InterfaceManage.models import ItInterface, ItInterfaceField, ItModule, ItProject, ItRecord
+
+admin.site.register(ItInterface)
+admin.site.register(ItInterfaceField)
+admin.site.register(ItModule)
+admin.site.register(ItProject)
+admin.site.register(ItRecord)
